fe5b190f292d49dc48b2feb5e3c7abdfde93985a
Jappie Klooster <jappieklooster@hotmail.com>
2022-05-02 21:19:11 -0400

    this contains somewhat working links

--- agda-search-web/src/AgdaSearch/Web.hs
+++ agda-search-web/src/AgdaSearch/Web.hs
@@ -9,8 +9,9 @@
 
 module AgdaSearch.Web(serve) where
 
+import System.FilePath
+import Text.Regex.Base()
 import AgdaSearch.Web.Settings
-import AgdaSearch.Uri
 import Data.Pool
 import Control.Monad
 import qualified Data.Text as Text
@@ -25,15 +26,26 @@ import Yesod.Form.Types
 import Yesod.Form.Functions
 import Yesod.Form.Fields
 import AgdaSearch.Query
+import qualified Text.Regex.Quote as Regex
+import Text.Regex.Posix (Regex)
+import Text.Regex.Lens
+import Control.Lens
+import qualified Data.Text.IO as Text
 
-data RoutedApp = MkRoutedApp { appSqlite :: Pool Connection }
+allDots :: Regex
+allDots = [Regex.r|\.|]
+
+data RoutedApp = MkRoutedApp
+  { appSqlite :: Pool Connection
+  , appSettings :: Settings
+  }
 
 instance RenderMessage RoutedApp FormMessage where
     renderMessage _ _ = defaultFormMessage
 
 mkYesod "RoutedApp" [parseRoutes|
 /                HomeR      GET
-/file/#Text/#Int FileR      GET
+/file/#Text FileR      GET
 |]
 
 searchForm :: Html -> MForm (Handler) (FormResult Text, Widget)
@@ -48,9 +60,28 @@ homeWidget :: Widget -> [Text] -> [(Identifier, Maybe ModRef)] -> Handler Html
 homeWidget searchWidget failures identifiers =
   appLayout $(widgetFileNoReload Default.def "home")
 
-getFileR :: Text -> Int -> Handler Html
-getFileR name offset =
-  redirect $ _1labFile name offset
+getFileR :: Text -> Handler Html
+getFileR name = do
+  $(logInfo) $ "looking up file: " <> name
+  app <- getYesod
+  let sources = agdaSources $ appSettings app
+      -- it doesn't work with text :S
+      slashed :: String -- Algebra.Relations.agda -> Algebra/Relations/agda
+      slashed = (regex allDots . matchedString .~ "/") $ Text.unpack name
+
+      postfixed :: String -- Algebra/Relations/agda ->  Algebra/Relations.agda
+      postfixed = slashed <> ".agda"
+
+  $(logInfo) $ "fixed up the module path: " <> Text.pack (show (slashed, postfixed))
+
+  let fullPath = sources </> postfixed
+
+  $(logInfo) $ "opening " <> Text.pack fullPath
+
+  res <- liftIO $ Text.readFile $ sources </> postfixed
+  appLayout $ [whamlet| kkk ^{res} |]
+
+  -- redirect $ _1labFile name offset
 
 getHomeR :: Handler Html
 getHomeR = do
@@ -80,12 +111,14 @@ getHomeR = do
 -- wtf?
 instance Yesod RoutedApp
 
-makeFoundation :: FilePath -> IO RoutedApp
-makeFoundation sqlitePath =
-  MkRoutedApp <$> createPool (open sqlitePath) close 1 5 10
+makeFoundation :: Settings -> IO RoutedApp
+makeFoundation settings =
+  MkRoutedApp <$> createPool (open (dbPath settings)) close 1 5 10
+              <*> pure settings
+
 
 serve :: Settings -> IO ()
 serve settings@MkSettings{..} = do
   print ("running with" :: String, settings)
-  x <- makeFoundation dbPath
+  x <- makeFoundation settings
   warp port x
--- nix/ci.nix
+++ nix/ci.nix
@@ -1,4 +1,5 @@
 {
   db = import ./create-db.nix {};
   server = import ../agda-search-web {};
+  agda-sources = (import ./pkgs.nix).agdaPackages.standard-library;
 }
