07d048f0f5c184e5866e0159ef2e533bbca20080
Jappie Klooster <jappieklooster@hotmail.com>
2022-05-03 20:30:36 -0400

    try using same pin, try using shakefile

--- nix/db/1lab.nix
+++ nix/db/1lab.nix
@@ -1,14 +1,28 @@
 {pkgs ? import ../pkgs.nix }:
 let
+  our-ghc = pkgs.haskellPackages.ghcWithPackages (pkgs: with pkgs; [
+    shake directory tagsoup
+    text containers uri-encode
+    process aeson Agda pandoc SHA
+  ]);
+  shakefile = import "${sources}/support/nix/build-shake.nix"
+    {
+      inherit our-ghc;
+      haskellPackages = pkgs.haskellPackages;
+      inherit (pkgs) removeReferencesTo stdenv;
+      name = "1lab-shake";
+      main = "Main.hs";
+    };
   sources = builtins.fetchGit {
     url = "https://github.com/plt-amy/1lab";
-    rev = "82394aebd120212bf0e21e441f3e2ed6318d1278";
+    rev = "e7f3da6c3ef5054c791abb6e26eccf097c6b6a59";
   };
 
 in
 pkgs.runCommand "search-index.db" {} ''
     set -xe
-    cp -R ${sources}/src/* ./
+    cp -R ${sources}/* ./
     chmod 700 -R ./*
-    "${pkgs.haskellPackages.agda-search}"/bin/agda-search ./ ./index.lagda.md $out --command createdb --cubical
+    ${shakefile}/bin/1lab-shake _build/all-pages.agda -j || echo "thx, we just needed all-pages.agda with appropriate modules"
+    "${pkgs.haskellPackages.agda-search}"/bin/agda-search ./ ./_build/all-pages.agda $out --command createdb --cubical
   ''
--- nix/pin.nix
+++ nix/pin.nix
@@ -1,6 +1,5 @@
-import (builtins.fetchGit {
-    # Descriptive name to make the store path easier to identify
-    name = "nixos-pin-2022.04.31";
-    url = "https://github.com/nixos/nixpkgs/";
-    rev = "e96c668072d7c98ddf2062f6d2b37f84909a572b";
-    })
+import (builtins.fetchTarball {
+  name = "1lab-nixpkgs-nixos-21.11";
+  url = "https://github.com/nixos/nixpkgs/archive/64fc73bd74f04d3e10cb4e70e1c65b92337e76db.tar.gz";
+  sha256 = "056dxf6zmsyapf9kx5qasdh3f0kkydbl2s9lb62gh2c51klsihrb";
+})
